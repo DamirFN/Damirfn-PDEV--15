fb104be2df6e07750c858f633e93eb4ab39121a3
DamirFN <dreamstarsd@gmail.com>
2023-05-02 12:43:01 +0700

    refresh

--- project/simpleapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Category, Product   # ипортируем категории(классы из models)
-
-admin.site.register(Category)
-admin.site.register(Product)
-# Register your models here.
