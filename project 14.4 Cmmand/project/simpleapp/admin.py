ce421fbd153818c4cd8555af73bd1c7bc561031a
DamirFN <dreamstarsd@gmail.com>
2023-06-16 17:28:42 +0700

    admin

--- project 14.4 Cmmand/project/simpleapp/admin.py
+++ project 14.4 Cmmand/project/simpleapp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import *    # импортируем категории(классы из models)
 
+# создаём новый класс для представления новостей в админке
+class NewsAdmin(admin.ModelAdmin):
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с новостями
+    list_display = [field.name for field in NewsPortal._meta.get_fields()]  # генерируем список имён всех
+    # полей для более красивого отображения
+    # list_display = ('article_title', 'article_author')
 
 class NewsCategoryInline(admin.TabularInline):
     model = NewsPortalCategory
@@ -14,6 +20,6 @@ class NewsPortalAdmin(admin.ModelAdmin):
 admin.site.register(NewsCategory)
 admin.site.register(NewsPortalCategory)
 admin.site.register(NewsPortal, NewsPortalAdmin)
-
+# admin.site.unregister(NewsPortal)   # разрегистрируем наши категории новостей
 
 # Register your models here.
